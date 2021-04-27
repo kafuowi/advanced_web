fa40244f1b821d076778240a5eb84ddb39299ee6
kafuowi <48575859+kafuowi@users.noreply.github.com>
2021-04-27 16:08:11 +0900

    Add files via upload

--- midterm/tictactoe/tictactoe.js
+++ midterm/tictactoe/tictactoe.js
@@ -1,6 +1,6 @@
 var cellList = document.querySelectorAll("td")
 var restart = document.querySelector("#restart_button")
-
+/*
 
 for( var i=0;i<9;i++){
     
@@ -15,5 +15,37 @@ for( var i=0;i<9;i++){
 restart.addEventListener("click",function(){
     for(var i=0;i<9;i++){
         cellList[i].textContent=""
+    }
+})*/
+var currentPlayer =1;
+
+$('.tictac button').on('click', function(){
+
+    var col = $(this).closest('td').index();
+    console.log($(this).text());
+    if($(this).text()==="O"||$(this).text()==="X")
+    {
+
+    }
+    else{
+        currentPlayer = currentPlayer * -1;
+
+        if(currentPlayer === 1){
+            $(this).text("O");
+        }else{
+            $(this).text("X");
+        }
+    }
+    
+
+    
+})
+
+var table = $('table tr');
+$('#restart_button').on('click',function(){
+    for(var col = 0; col<3;col++){
+        for(var row=0; row<3;row++){
+            table.eq(row).find('td').eq(col).find('button').text('');
+        }
     }
 })
